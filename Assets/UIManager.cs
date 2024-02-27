19b478d5fbe57ee75257bc1d93ab0c66fc9c0457
Dylan <dylanryan431@gmail.com>
2024-02-27 10:26:55 -0400

    almost

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -8,6 +8,10 @@ public class UIManager : MonoBehaviour
 {
     public TextMeshProUGUI text;
     int score;
+    public GameObject RGBImage;
+    public Slider redSlider;
+    public Slider greenSlider;
+    public Slider blueSlider;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,25 +25,45 @@ public class UIManager : MonoBehaviour
         
     }
 
-    public void ScoreUpdate(string output)
+    public void QuitApp()
     {
-        if (output == "down")
-        {
-            score -= 1;
-            Debug.Log("Score Down");
-        }
+        Application.Quit();
+    }
 
-        if (output == "up")
-        {
-            score += 1;
-            Debug.Log("Score Up");
-        }
-
-        if (output == "reset")
+    public void ScoreUpdate(int value)
+    {
+        score += value;
+        score = Mathf.Clamp(score,0, 999999999);
+        if (value == 0)
         {
             score = 0;
-            Debug.Log("Score Reset");
         }
-        text.GetComponent<TextMeshProUGUI>().text = score.ToString();
+        text.GetComponent<TextMeshProUGUI>().text = score.ToString("N0");
+    }
+
+    public void ScoreRandom()
+    {
+        score += Random.Range(0, 999999999);
+        score = Mathf.Clamp(score, 0, 999999999);
+        text.GetComponent<TextMeshProUGUI>().text = score.ToString("N0");
+    }
+
+    public void ColorChange(string color)
+    {
+        Color currentColor = RGBImage.GetComponent<Image>().color;
+        if (color == "red")
+        {
+            RGBImage.GetComponent<Image>().color = Color.Lerp(currentColor, Color.red, redSlider.value / 100);
+        }        
+        if (color == "green")
+        {
+            RGBImage.GetComponent<Image>().color = Color.Lerp(currentColor, Color.green, greenSlider.value / 100);
+        }
+        if (color == "blue")
+        {
+            RGBImage.GetComponent<Image>().color = Color.Lerp(currentColor, Color.red, blueSlider.value / 100);
+        }
+
+
     }
 }
